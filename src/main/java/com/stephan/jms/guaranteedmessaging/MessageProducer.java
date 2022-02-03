fd06e2c7ac66580dc2154c28794c21f065bd3985
Stephan Fernandes <stephanfernandes4@gmail.com>
2022-02-03 15:06:34 +0530

    jms gurantedmsg

---
+++ src/main/java/com/stephan/jms/guaranteedmessaging/MessageProducer.java
@@ -0,0 +1,39 @@
+package com.stephan.jms.guaranteedmessaging;
+
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.JMSProducer;
+import javax.jms.Queue;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class MessageProducer {
+
+	public static void main(String[] args) throws NamingException, JMSException {
+		InitialContext initialContext = new InitialContext();
+		Queue requestQueue = (Queue) initialContext.lookup("queue/requestQueue");
+		
+
+//		try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+//				JMSContext jmsContext=cf.createContext(JMSContext.DUPS_OK_ACKNOWLEDGE)){
+			
+			try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+					JMSContext jmsContext=cf.createContext(JMSContext.SESSION_TRANSACTED)){
+				
+			JMSProducer producer = jmsContext.createProducer();
+			
+			producer.send(requestQueue, "Message 1");
+			
+			producer.send(requestQueue, "Message 2");
+			jmsContext.commit();
+			//jmsContext.commit();
+			//jmsContext.rollback();
+			//object msg
+			
+		}
+
+	}
+
+}
